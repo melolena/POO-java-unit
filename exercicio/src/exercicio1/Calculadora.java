d2cc7c08faa2c26e41544d80bb4366cb5775bb2d
melolena <milenasantosmelo@gmail.com>
2024-02-29 11:21:28 -0300

    aula 3

---
+++ exercicio/src/exercicio1/Calculadora.java
@@ -0,0 +1,18 @@
+package exercicio1;
+
+public class Calculadora {
+	int operando1;
+	 int operando2;
+	 int adicao() {
+	 return operando1 + operando2;
+	 }
+	 int subtracao() {
+	 return operando1 - operando2;
+	 }
+	 int multiplicacao() {
+	 return operando1 * operando2;
+	 }
+	 int divisao() {
+	 return operando1 / operando2;
+	 }
+}
